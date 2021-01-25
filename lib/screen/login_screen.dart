877f07498ef6cebeac91c8a2827788635db97b0e
dinuchriist <lahiruzone@gmail.com>
2021-01-25 09:55:07 +0530

    bugfix/firebase

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:skype_app/resources/firebase_repository.dart';
@@ -12,56 +10,55 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScrrenState extends State<LoginScreen> {
-FirebasebaseRepository _repository = FirebasebaseRepository();
+  FirebasebaseRepository _repository = FirebasebaseRepository();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body:loginButton(),
-              );
-            }
-          
+      body: loginButton(),
+    );
+  }
+
   Widget loginButton() {
     return FlatButton(
       padding: EdgeInsets.all(35),
-      child: Text("LOGIN",
-       style: TextStyle(
-         fontSize: 35,
-         fontWeight: FontWeight.w900,
-         letterSpacing: 1.2,
-
-       ),
+      child: Text(
+        "LOGIN",
+        style: TextStyle(
+          fontSize: 35,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 1.2,
+        ),
       ),
-      onPressed: () => performLogin,
+      onPressed: () => performLogin(),
     );
   }
-void performLogin(){
-  _repository.signIn().then((user){
-if (user != null)
-{
-  authenticateUser(user);
-}else{
-  print('error is occured');
-}
-  });
 
-}
-
-void authenticateUser(FirebaseUser user){
-_repository.authenticateUser(user).then((isNewUser){
-  if(isNewUser){
-    _repository.addDataToDb(user).then((value){
-      Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context){
-          return HomePage();
-        })
-      );   
-    }
-);
-  }else {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
-          return HomePage();}));
+  void performLogin() {
+    _repository.signIn().then((user) {
+      if (user != null) {
+        authenticateUser(user);
+      } else {
+        print('error is occured');
+      }
+    });
   }
-});
-}
+
+  void authenticateUser(FirebaseUser user) {
+    _repository.authenticateUser(user).then((isNewUser) {
+      if (isNewUser) {
+        _repository.addDataToDb(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+            return HomePage();
+          }));
+        });
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+          return HomePage();
+        }));
+      }
+    });
+  }
 }
